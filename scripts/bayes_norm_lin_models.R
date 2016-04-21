f66563befb2ced3faed1b6b85ad47b5b5f30fc1f
Pavel <a.headache@seznam.cz>
2016-04-21 20:46:34 +0200

    anova analysis start added

--- scripts/bayes_norm_lin_models.R
+++ scripts/bayes_norm_lin_models.R
@@ -2,8 +2,10 @@ rm(list = ls())
 library(devtools)
 library(arm)
 library(checkpoint)
-checkpoint (snapshotDate = "2016-04-01", use.knitr = TRUE, auto.install.knitr = TRUE)
-#bayes play
+checkpoint (snapshotDate = "2016-04-01", use.knitr = TRUE, 
+            auto.install.knitr = TRUE)
+
+## Bayes play
 ## Fitting a linear regression
 n <- 50
 # set.seed(500) #in case you need same result every time
@@ -21,7 +23,7 @@ mod
 summary(mod)$sigma
 
 #drawing conclusions
-nsim <- 10000
+nsim <- 100000
 bsim <- arm::sim(mod, n.sim=nsim)
 apply (coef(bsim), 2, quantile, prob=c(0.025,0.975))
 quantile (bsim@sigma, prob = c(0.025,0.975))
@@ -31,4 +33,42 @@ sum(coef(bsim)[,2]>0.5)/nsim
 plot (x,y, pch=16, las=1, cex.lab=1.2)
 for (i in 1:nsim) abline (coef(bsim)[i,1], coef(bsim)[i,2], col=rgb(0,0,0,0.05))
 
-str(bsim)
+newdat <- data.frame (x=seq(10,30,0.1))
+newmodmat <- model.matrix (~x,data=newdat)
+fitmat <- matrix (ncol=nsim,nrow=nrow(newdat))
+for (i in 1:nsim) fitmat[,i] <- newmodmat %*% coef(bsim)[i,]
+plot(x,y)
+abline(mod,lwd=2)
+lines (newdat$x, apply(fitmat,1,quantile, prob=0.025), lty=3)
+lines (newdat$x, apply(fitmat,1,quantile, prob=0.975), lty=3)
+
+#prepare matrix for simulated data
+newy <- matrix (ncol=nsim, nrow=nrow (newdat))
+#for each simulated fitted value, simulate one new y-value
+for (i in 1:nsim) newy[,i] <- rnorm(nrow(newdat), mean=fitmat[,i], sd=bsim@sigma[i])
+lines (newdat$x, apply(newy,1,quantile, prob=0.025), lty=3)
+lines (newdat$x, apply(newy,1,quantile, prob=0.975), lty=3)
+
+
+##One-Way ANOVA
+
+mu <- 12
+sigma <- 2
+b1 <- 3
+b2 <- -5
+n <- 90
+group <- factor(rep(c(1,2,3), each=30))
+
+simresid <- rnorm(n, mean=0, sd=sigma)
+y <- mu+as.numeric(group=="2")*b1+as.numeric(group=="3")*b2+simresid
+
+group <- factor(group)
+mod <- lm(y~group)
+summary(mod)$sigma
+
+bsim <- arm::sim(mod, n.sim=1000)
+m.g1 <- coef(bsim)[,1]
+m.g2 <- coef(bsim)[,1]+coef(bsim)[,2]
+m.g3 <- coef(bsim)[,1]+coef(bsim)[,3]
+
+hist (coef(bsim),breaks= 100)
